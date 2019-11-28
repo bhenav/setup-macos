5c94c7c81659ae5bc01efab6ac0194251fa6593f
kutbettin <yavuz@kutbettin.com>
2019-11-29 00:56:08 +0300

    Update iterm.sh

--- iterm.sh
+++ iterm.sh
@@ -3,6 +3,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 brew tap caskroom/fonts
 brew cask install font-hack-nerd-font
 cp zshrc.conf ~/.zshrc
+source ~/.zshrc
 open -a iterm
 cp profile.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles
 brew install autojump
