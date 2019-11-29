cac640f9678b965b855283e5c8e861fbb3027a0b
kutbettin <yavuz@kutbettin.com>
2019-11-29 21:38:26 +0300

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -4,6 +4,14 @@ nvm install stable
 node -v
 
 # Global packages
-npm install -g yarn
-npm install -g @angular/cli
+npm i -g @angular-devkit/schematics-cli
+npm i -g @angular/cli
+npm i -g @nestjs/cli
+npm i -g node-gyp
+npm i -g replace
+npm i -g serve
+npm i -g http-server
+npm i -g typeorm
+npm i -g yarn
+npm i -g npm-check-updates
 ng set --global packageManager=yarn
